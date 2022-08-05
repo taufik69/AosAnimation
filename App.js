76e56f85867a922745ff3a4eb1381d710519180b
taufik69 <taufik35-2915@diu.edu.bd>
2022-08-05 19:48:07 +0600

    Aos animation

---
+++ App.js
@@ -0,0 +1,53 @@
+import './style.css'
+function App() {
+  return (
+    <>
+      <div className="pc" data-aos="flip-left">
+        <h1>taufk islam</h1>
+        <h2>Daffodil international univarsity</h2>
+        <h3>Full stack developer</h3>
+      </div>
+
+
+
+      <div className="pc" data-aos="flip-down">
+        <h1>taufk islam</h1>
+        <h2>Daffodil international univarsity</h2>
+        <h3>Full stack developer</h3>
+      </div>
+
+
+
+
+
+
+      <div className="pc" data-aos="flip-up">
+        <h1>taufk islam</h1>
+        <h2>Daffodil international univarsity</h2>
+        <h3>Full stack developer</h3>
+      </div>
+
+
+
+
+      <div className="pc" data-aos="fade-down-right">
+        <h1>taufk islam</h1>
+        <h2>Daffodil international univarsity</h2>
+        <h3>Full stack developer</h3>
+      </div>
+
+
+
+
+
+      <div className="pc" ddata-aos="fade-right">
+        <h1>taufk islam</h1>
+        <h2>Daffodil international univarsity</h2>
+        <h3>Full stack developer</h3>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
